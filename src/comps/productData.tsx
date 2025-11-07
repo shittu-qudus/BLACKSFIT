d028d9d3cdc6c1620653672667f9cdb82733e142
SHITT QUDUS.A <shittuqadekunle@gmail.com>
2025-11-07 02:33:57 -0800

    Ibadan piece removed

--- src/comps/productData.tsx
+++ src/comps/productData.tsx
@@ -6,7 +6,7 @@ export const productData: Product[] = [
      { id: 2, name: "Eyo Adimu piece", photoUrl: "/image/eyofront.png", fullimage:"/image/eyo.jpeg", size: 44, price: 40000 },
     // { id: 4, name: "Blacksfit Peice III", photoUrl: "/image/blank.jpeg", fullimage:"/image/FULLATL.jpg",size: 44, price: 35000 },
 
-    { id: 6, name: "Ibadan brown roof piece", photoUrl: "/image/ibadan.png",fullimage:"/image/FULLIB.jpg", size: 44, price: 40000 },
+    // { id: 6, name: "Ibadan brown roof piece", photoUrl: "/image/ibadan.png",fullimage:"/image/FULLIB.jpg", size: 44, price: 40000 },
 
    
     { id: 9, name: "Kwara state piece", photoUrl: "/image/map.jpeg", fullimage:"/image/mapk.jpeg",size: 44, price: 40000 },
@@ -15,4 +15,5 @@ export const productData: Product[] = [
     { id: 12, name: "Kurmi piece ", photoUrl: "/image/kurmifront.png",fullimage:"/image/newfullk.png", size: 44, price: 40000 },
 
 ]
+
 // console.log('Product image URLs:', productData.map(p => ({ name:p.name, url: p.photoUrl }))) //
